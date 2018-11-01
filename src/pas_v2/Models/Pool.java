dec4582c61287613421f9508d1e6b575ff2dcacf
Dmitry Mikhailov <dfm5466@psu.edu>
2018-11-01 19:14:46 +0300

    minor revisions

--- src/pas_v2/Models/Pool.java
+++ src/pas_v2/Models/Pool.java
@@ -1,11 +1,21 @@
 package pas_v2.Models;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.PrintWriter;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import pas_v2.Models.Reports.Report;
 
 /**
@@ -19,131 +29,118 @@ public class Pool {
     ArrayList<Swimmer> activeSwimmers;
     ArrayList<ActiveSwimmerData> activePool;
     private ArrayList<Visit> visits;
-    
-    //serializeable files
-    private String listOfSwimmersFileName = "swimmers.ser";
-    
+    private Gson gson;
+    private Type listType;
 
+    //serializeable files
+    private String listOfSwimmersFileName = "swimmers";
 
     public Pool() {
         swimmers = new ArrayList<>();
         visits = new ArrayList<>();
         report = new Report(this);
         activeSwimmers = new ArrayList<>();
-        activePool = new ArrayList<>();
+        activePool = new ArrayList<>();        
+        gson = new Gson();
+        listType = new TypeToken<ArrayList<Swimmer>>() {}.getType();
         
-                this.readSwimmerListFile();
+        this.readSwimmerListFile();
 
-                if(swimmers.isEmpty() || swimmers == null){
+        if (swimmers.isEmpty() || swimmers == null) {
 
-                    this.writeSwimmerListFile();
-                    this.readSwimmerListFile();
+            this.writeSwimmerListFile();
+            this.readSwimmerListFile();
 
-                }
-        
+        }
+
         //printSwimmerList();
     }
     //selectedSwimmer, updatedSwimmer
-    
-    public void constructActivePool(ArrayList<Swimmer> activeSwimmers){
+
+    public void constructActivePool(ArrayList<Swimmer> activeSwimmers) {
         activePool.clear();
-        for(int i = 0; i < activeSwimmers.size(); i++){
+        for (int i = 0; i < activeSwimmers.size(); i++) {
             ActiveSwimmerData data = new ActiveSwimmerData(activeSwimmers.get(i));
             //data.fillData(activeSwimmers.get(i));
             activePool.add(data);
         }
-        
+
     }
-    
-    public void clearActiveSwimmers(){
+
+    public void clearActiveSwimmers() {
         this.activeSwimmers.clear();
         this.activePool.clear();
     }
-    
-    public void updateSwimmer(Swimmer originalSwimmer, Swimmer updatedSwimmer){
+
+    public void updateSwimmer(Swimmer originalSwimmer, Swimmer updatedSwimmer) {
         swimmers.set(swimmers.indexOf(originalSwimmer), updatedSwimmer);
-        
+
     }
-    
-    public void changeSwimmerStatus(Swimmer tempSwimmer, boolean status){
+
+    public void changeSwimmerStatus(Swimmer tempSwimmer, boolean status) {
         swimmers.get(swimmers.indexOf(tempSwimmer)).setCheckedStatus(status);
-        
+
         this.writeSwimmerListFile();
     }
-    
-    public void deleteSwimmer(Swimmer swimmer){
+
+    public void deleteSwimmer(Swimmer swimmer) {
         swimmers.remove(swimmer);
         swimmers.trimToSize();
     }
-    
-    public ArrayList<Swimmer> searchSwimmer(String keyword){
+
+    public ArrayList<Swimmer> searchSwimmer(String keyword) {
         ArrayList<Swimmer> tempList = new ArrayList<>();
-        
-        for(Swimmer s : swimmers){
-            if(s.getSwimmerInformation().toLowerCase().contains(keyword.toLowerCase())){
+
+        for (Swimmer s : swimmers) {
+            if (s.getSwimmerInformation().toLowerCase().contains(keyword.toLowerCase())) {
                 tempList.add(s);
             }
         }
-        
+
         return tempList;
     }
-    
-    public ArrayList<ActiveSwimmerData> searchActiveSwimmers(String keyword){
+
+    public ArrayList<ActiveSwimmerData> searchActiveSwimmers(String keyword) {
         ArrayList<ActiveSwimmerData> tempList = new ArrayList<>();
-        
-        for(Swimmer tempActive : this.activeSwimmers){
-            if(tempActive.getSwimmerInformation().toLowerCase().contains(keyword.toLowerCase())){
-                
-                
+
+        for (Swimmer tempActive : this.activeSwimmers) {
+            if (tempActive.getSwimmerInformation().toLowerCase().contains(keyword.toLowerCase())) {
+
                 ActiveSwimmerData tempSwimmer = new ActiveSwimmerData(tempActive);
                 tempSwimmer.setVisits(tempActive.getVisits());
                 tempSwimmer.setNote(tempActive.getNote());
-            
-                
+
                 tempList.add(tempSwimmer);
             }
         }
-        
+
         return tempList;
     }
-    
-    public void readSwimmerListFile(){
-        FileInputStream fis = null;
-        ObjectInputStream in = null;
-        try {
-            fis = new FileInputStream(listOfSwimmersFileName);
-            in = new ObjectInputStream(fis);
-            swimmers = (ArrayList)in.readObject();
-            in.close();
-//            if(!swimmers.isEmpty()){
-//                System.out.println("There are swimmers in the swimmer list");
-//            }
-        }
-        catch(IOException ex){
-            System.out.println(listOfSwimmersFileName + " not found, creating.");
-        }
-        catch(ClassNotFoundException ex){
+
+    public void readSwimmerListFile() {      
+        try (Scanner input = new Scanner(new File(listOfSwimmersFileName + ".json"))){           
+            String stringJSON = input.useDelimiter("\\A").next();
+            swimmers = gson.fromJson(stringJSON, listType); 
+        } catch (FileNotFoundException ex) {
+            System.out.println(listOfSwimmersFileName + ".json not found, creating.");
+        }       
+    }
+
+    public void writeSwimmerListFile() {        
+        String stringJSON = gson.toJson(swimmers, listType);
+        try (PrintWriter output = new PrintWriter(listOfSwimmersFileName + ".json")) {
+            output.println(stringJSON);
+            output.close();
+
+        } catch (FileNotFoundException ex) {
             ex.printStackTrace();
         }
+
     }
-    
-    public void writeSwimmerListFile(){
-        FileOutputStream fos = null;
-        ObjectOutputStream out = null;
-        try {
-            fos = new FileOutputStream(listOfSwimmersFileName);
-            out = new ObjectOutputStream(fos);
-            out.writeObject(swimmers);
-            out.close();
-        }
-        catch(IOException ex){
-            ex.printStackTrace();
-        }
-    }
-    
-    public void printSwimmerList(){
+
+    public void printSwimmerList() {
         System.out.println("The SwimmerList has these swimmers:");
-        for(int i = 0; i < swimmers.size(); i++){
+        for (int i = 0; i < swimmers.size(); i++) {
             Swimmer currentUser = (Swimmer) swimmers.get(i);
             System.out.println(currentUser.getSwimmerInformation());
         }
@@ -170,29 +167,29 @@ public class Pool {
     /**
      * @return only swimmers in an active status
      */
-    public ArrayList<Swimmer> getActiveSwimmers(){
+    public ArrayList<Swimmer> getActiveSwimmers() {
         activeSwimmers.removeAll(swimmers);
-        for(int i=0; i<swimmers.size(); i++){
-            if(swimmers.get(i).getCheckedStatus().equals("Checked in")){
+        for (int i = 0; i < swimmers.size(); i++) {
+            if (swimmers.get(i).getCheckedStatus().equals("Checked in")) {
                 activeSwimmers.add(swimmers.get(i));
-            }   
+            }
         }
         return activeSwimmers;
     }
-    
+
     /**
      * @return the visits
      */
     public ArrayList<Visit> getVisits() {
         return visits;
     }
-    
-    public void addSwimmer(Swimmer swimmer){
+
+    public void addSwimmer(Swimmer swimmer) {
         swimmers.add(swimmer);
     }
-    
-    public void addVisit(Swimmer swimmer){
+
+    public void addVisit(Swimmer swimmer) {
         visits.add(new Visit(swimmer));
     }
-    
+
 }
