94fc4198d622827b649a759b8128ed1a359c18fd
Drew <abh5595@psu.edu>
2018-11-07 10:03:48 -0500

    created UI for sprint 5 - staff

---
+++ src/pas_v2/Controllers/EditEmpPopupController.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pas_v2.Controllers;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author andre
+ */
+public class EditEmpPopupController implements Initializable {
+    @FXML Button cnclBtn;
+    @FXML Button saveBtn;
+    @FXML TextField fNameTxtField;
+    @FXML TextField lNameTxtField;
+    @FXML ToggleGroup roleSelector;
+    @FXML RadioButton rbAdmin;
+    @FXML RadioButton rbOperator;
+
+    
+    private String decision;
+    
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }
+
+    public void saveBtnClicked(ActionEvent event) throws IOException{
+        decision = "yes";
+        Stage popup = (Stage)((Node)event.getSource()).getScene().getWindow();
+        popup.close();
+    }
+    
+    public void cnclBtnClicked(ActionEvent event) throws IOException{
+        decision = "no";
+        Stage popup = (Stage)((Node)event.getSource()).getScene().getWindow();
+        popup.close();
+    }
+
+    public String getDecision() {
+        return decision;
+    }
+}
--- src/pas_v2/Controllers/StaffUIController.java
+++ src/pas_v2/Controllers/StaffUIController.java
@@ -19,6 +19,7 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import pas_v2.Models.Employee;
 import pas_v2.Models.EmployeeList;
@@ -27,11 +28,13 @@ import pas_v2.Models.RoleEnum;
 /**
  * FXML Controller class
  *
- * @author David Ortiz
+ * @author David Ortiz, Drew Hopkins
  */
 public class StaffUIController implements Initializable {
 
-    @FXML Button newEmployeeButton;
+    @FXML Button newEmpBtn;
+    @FXML Button editBtn;
+    @FXML Button removeBtn;
     
     private Employee currentEmployee;
     private EmployeeList employeeList;
@@ -55,13 +58,11 @@ public class StaffUIController implements Initializable {
         this.currentEmployee = emp;
         this.employeeList = employeeList;
         
-
-        newEmployeeButton.setDisable(!currentEmployee.isFunctionPermitted(RoleEnum.CREATE_EMPLOYEE));
-       
+        newEmpBtn.setDisable(!currentEmployee.isFunctionPermitted(RoleEnum.CREATE_EMPLOYEE));
     }
    
     
-    public void NewEmployeeButtonClicked(ActionEvent event) throws IOException{
+    public void newEmpBtnClicked(ActionEvent event) throws IOException{
         
         
         FXMLLoader loader = new FXMLLoader();
@@ -82,10 +83,66 @@ public class StaffUIController implements Initializable {
         
     }
     
+    /**
+     * Directs user to employee edit screen to change password or privileges 
+     * @param event occurs when edit button is clicked
+     * @throws IOException 
+     */
+    public void editEmpBtnClicked(ActionEvent event) throws IOException{
+        
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("/pas_v2/Views/EditEmpPopup.fxml"));
+        Parent tableViewParent = loader.load();
+        
+        Scene scene = new Scene(tableViewParent);
+        
+        //access the controller and call a method
+        EditEmpPopupController controller = loader.getController();
+        
+        //This line gets the Stage information
+        Stage window = new Stage();
+        window.initModality(Modality.APPLICATION_MODAL);
+        window.initOwner((Stage)((Node)event.getSource()).getScene().getWindow());
+        window.setScene(scene);
+        window.showAndWait();
+        
+        if (controller.getDecision().equals("yes")){
+            
+            //TODO: create remove employee logic, only admins can remove
+            
+        }
+    }
     
+    /**
+     * Removes an employee from the list of employees
+     * @param event occurs when remove button is clicked
+     * @throws IOException 
+     */
+    public void removeEmpBtnClicked (ActionEvent event) throws IOException{
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("/pas_v2/Views/RemoveEmpPopup.fxml"));
+        Parent tableViewParent = loader.load();
+        
+        Scene scene = new Scene(tableViewParent);
+        
+        //access the controller and call a method
+        RemoveEmpPopoupController controller = loader.getController();
+        
+        //This line gets the Stage information
+        Stage window = new Stage();
+        window.initModality(Modality.APPLICATION_MODAL);
+        window.initOwner((Stage)((Node)event.getSource()).getScene().getWindow());
+        window.setScene(scene);
+        window.showAndWait();
+        
+        if (controller.getDecision().equals("yes")){
+            
+            //TODO: create remove employee logic, only admins can remove
+            
+        }
+    }
     
-    public void navigateToMainMenu(ActionEvent event) throws IOException
-    {
+    public void navigateToMainMenu(ActionEvent event) throws IOException{
         FXMLLoader loader = new FXMLLoader();
         loader.setLocation(getClass().getResource("/pas_v2/Views/MainMenuUI.fxml"));
         Parent tableViewParent = loader.load();
@@ -102,7 +159,21 @@ public class StaffUIController implements Initializable {
         
         window.setScene(tableViewScene);
         window.show();
-        
     }
     
+    public void userClickedTable(){
+        removeBtn.setDisable(false);
+        editBtn.setDisable(false);
+    }
+    
+    public void performSearch(){
+        
+
+        //TODO
+
+
+        //        String keyword = searchField.getText();
+        //        pool.constructActivePool(pool.getActiveSwimmers());
+        //        tableView.getItems().setAll(pool.searchActiveSwimmers(keyword));
+    }
 }
